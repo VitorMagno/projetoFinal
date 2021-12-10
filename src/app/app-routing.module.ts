816230b8f7cc38ff407729881ec095e76ff047b8
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2021-12-10 19:24:16 -0300

    adiciona todo  conteudo dos templatees

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { ListarComponent } from './listar/listar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo:'/inicio', pathMatch:'full'},
+  { path: 'inicio', component: AppComponent},
+  { path: 'listar', component: ListarComponent},
+  { path: 'cadastrar', component: CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
