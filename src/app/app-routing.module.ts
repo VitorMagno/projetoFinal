6b5c1dca15e7ced8e8d84955aa78303257a49b2d
Vitor Magno C. de Gouveia <45520686+VitorMagno@users.noreply.github.com>
2021-12-11 19:17:15 -0300

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AbGeralComponent } from './ab-geral/ab-geral.component';
+import { AbItensComponent } from './ab-itens/ab-itens.component';
+import { AbProjetosComponent } from './ab-projetos/ab-projetos.component';
 import { AppComponent } from './app.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { ListarComponent } from './listar/listar.component';
@@ -8,7 +11,10 @@ const routes: Routes = [
   { path: '', redirectTo:'/inicio', pathMatch:'full'},
   { path: 'inicio', component: AppComponent},
   { path: 'listar', component: ListarComponent},
-  { path: 'cadastrar', component: CadastrarComponent}
+  { path: 'cadastrar', component: CadastrarComponent},
+  { path: 'cadastrar/geral', component: AbGeralComponent},
+  { path: 'cadastrar/itens', component: AbItensComponent},
+  { path: 'cadastrar/projetos', component: AbProjetosComponent}
 ];
 
 @NgModule({
